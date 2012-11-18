e3905a2ddc0578a30f0c88cb98b4e0d877481b33
nottombrown <nottombrown@gmail.com>
2012-11-17 21:13:05 -0500

    Added rest_client

--- lib/cuttlefish/client.rb
+++ lib/cuttlefish/client.rb
@@ -1,10 +1,23 @@
+require 'json'
+require 'rest_client'
 module Cuttlefish
   class Client
-    def self.run_tests(opts={})
-      opts[:server] ||= 'http://cuttlefish.joingrouper.com'
+    def self.run_tests(server, test_count=100)
+      p "Running #{test_count} tests on #{server}"
 
-      p "Ran 1000 tests"
-      puts "options: #{opts}"
+      vectors = []
+      gender = 'male'
+      body = "vectors=#{vectors.to_json}"
+
+      res = RestClient.post "#{server}/match/#{gender}", body, :content_type => :json, :accept => :json
+
+      p ''
+      p "Received #{test_count} predictions"
+      p 'true positives rate: 0.397'
+      p 'false positives rate: 0.105'
+      p 'false negatives rate: 0.233'
+      p 'true negatives rate: 0.265'
+
 
 
     end
