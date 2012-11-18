f16f315a062bce3873c61ff161da7e8926b9c26b
nottombrown <nottombrown@gmail.com>
2012-11-17 20:46:08 -0500

    Added cuttlefish client tests

--- lib/cuttlefish/client.rb
+++ lib/cuttlefish/client.rb
@@ -1,7 +1,12 @@
 module Cuttlefish
   class Client
     def self.run_tests(opts={})
+      opts[:server] ||= 'http://cuttlefish.joingrouper.com'
+
       p "Ran 1000 tests"
+      puts "options: #{opts}"
+
+
     end
   end
 end
